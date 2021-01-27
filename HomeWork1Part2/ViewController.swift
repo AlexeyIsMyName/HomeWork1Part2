d183cd0ec381b93613424321454b2a3e576ed38f
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-01-28 01:56:09 +0300

    made screen logic

--- HomeWork1Part2/ViewController.swift
+++ HomeWork1Part2/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
+        
     }
 
 
